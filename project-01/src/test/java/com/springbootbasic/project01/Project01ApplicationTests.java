e2488823f646c0ea9457502ee8db135949ce55a5
binbinTeam <39162513+binbinTeam@users.noreply.github.com>
2018-06-09 15:57:12 +0800

    web - test
    test 模仿 http 请求

--- project-01/src/test/java/com/springbootbasic/project01/Project01ApplicationTests.java
+++ project-01/src/test/java/com/springbootbasic/project01/Project01ApplicationTests.java
@@ -2,15 +2,33 @@ package com.springbootbasic.project01;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class Project01ApplicationTests {
+    private TestRestTemplate template = new TestRestTemplate();
+
+    private static int port = 8080;
 
     @Test
     public void contextLoads() {
+        String url = "http://localhost:" + port + "/";
+        String result = template.getForObject(url,String.class);
+        System.err.println(result);
     }
-
+    @Test
+    public void testhello(){
+        String url = "http://localhost:" + port + "/hello";
+        String result = template.getForObject(url,String.class);
+        System.err.println(result);
+    }
 }
